ce8ce2fda81f6b6e81f856dee93d24b39eeb0b25
Deeeeeee <445214770@qq.com>
2016-10-03 10:19:43 +0800

    update

---
+++ static/js/common.js
@@ -0,0 +1,22 @@
+requirejs.config({
+    //baseUrl: './',
+    //urlArgs: "version=" + new Date().getTime(),
+    paths: {
+        jquery: 'lib/jquery.min',
+        //pagination: 'vendor/jquery.page',
+        //remodal: 'vendor/remodal',
+        //tab: 'vendor/jquery.tab',
+        //echarts: 'vendor/echarts.min',
+        //datepicker: 'vendor/jquery.datetimepicker',
+        //carousel: 'vendor/jquery.carousel',
+        //footer: 'vendor/locateFooter',
+        //Validform: 'vendor/Validform',
+    },
+    shim: {
+
+    }
+});
+
+define(["jquery"], function () {
+
+});
---
+++ static/js/page/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by DAY on 16/10/2.
+ */
+define([
+    "jquery",
+    //"swiper",
+],function($){
+    var page = {
+        init: function() {
+            this.onCheckLogin()
+        },
+        onCheckLogin: function () {
+            $.ajax({
+                type: "get",
+                url: "http://www.homeownership.cn/editUser.action",
+                data: {
+                    "password":"123456",
+                    "phone":"13390517165",
+                    id:"1"
+                },
+                dataType: "json",
+                success: function(data){
+                    console.log(data)
+                }
+            });
+        }
+    };
+    page.init();
+});
