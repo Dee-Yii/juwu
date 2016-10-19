2ae3221596ff6b9ec72993b2a6c6e789b0082068
Deeeeeee <445214770@qq.com>
2016-10-19 15:39:35 +0800

    banner&userCenter

--- zhstatic/js/page/userCenter.js
+++ zhstatic/js/page/userCenter.js
@@ -4,7 +4,8 @@
 
     var page = {
         init: function() {
-            this.onCheckLogin()
+            this.onCheckLogin();
+            this.onChangeType();
         },
         onCheckLogin: function () {
             $.ajax({
@@ -25,6 +26,25 @@
                 $(".person-center").show();
             },function (){
                 $(".person-center").hide();
+            })
+        },
+        onChangeType: function () {
+            var btn = $(".J_type");
+            var urlArr = [
+                "http://www.homeownership.cn/zd/work_publish.action",
+                "http://www.homeownership.cn/zd/article_publish.action",
+                "http://www.homeownership.cn/zd/activity_publish.action",
+                "http://www.homeownership.cn/zd/task_publish.action",
+                "http://www.homeownership.cn/zd/recruitment_publish.action",
+                //"http://www.homeownership.cn/zd/task.action",
+            ]
+            btn.on("click", function () {
+                var $this = $(this);
+                var txt = $this.find(".type").text();
+                var index = $this.index();
+                $this.addClass("active").siblings().removeClass("active");
+                $(".J_pubLink").text("发布" + txt).attr("href",urlArr[index]);
+
             })
         }
     };
